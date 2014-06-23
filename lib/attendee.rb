815ccc84dfe722273f7e7d4d84631ead8ae8f5a7
Hilary Denton <Hilarydenton90@gmail.com>
2014-06-23 13:12:58 -0600

    Cleans data

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -13,18 +13,40 @@ class Attendee
   def initialize(data)
     @id = data[:id]
     @regdate = data[:regdate]
-    @first_name = data[:first_name]
-    @last_name = data[:last_name]
+    @first_name = clean_first_name(data[:first_name])
+    @last_name = clean_last_name(data[:last_name])
     @email_address = data[:email_address]
-    @homephone = data[:homephone]
+    @homephone = clean_homephone(data[:homephone])
     @street = data[:street]
     @city = data[:city]
     @state = data[:state]
-    @zipcode = data[:zipcode]
+    @zipcode = clean_zipcode(data[:zipcode])
   end
 
   # clean all the things
+  def clean_first_name(first_name)
+    first_name.upcase
+  end
 
-  def clean_zipcode
+  def clean_last_name(first_name)
+    last_name.upcase
+  end
+
+  def clean_zipcode(zipcode)
+    zipcode.to_s.rjust(5,"0")[0..4]
+  end
+
+  def clean_homephone
+    if phone_number.nil? || phone_number.length < 10
+      phone_number = "0000000000"
+    elsif phone_number.length == 10
+      phone_number = phone_number[0..9]
+    elsif phone_number.length == 11
+      if phone_number[0] == "1"
+         phone_number = phone_number[1..10]
+      elsif phone_number[0] != "1"
+         phone_number = "0000000000"
+      end
+    end
   end
 end
