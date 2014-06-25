f05e7a7784357b017bac252143a11e32ae282d68
Hilary Denton <Hilarydenton90@gmail.com>
2014-06-25 16:38:09 -0600

    add parser class

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,20 +1,24 @@
 require 'pry'
 class CLI
-  attr_reader :command,
-              :parameters,
-              :queue_command,
-              :find_command,
-              :event_reporter
+  attr_reader   :command,
+                :parameters,
+                :queue_command,
+                :find_command,
+                :event_reporter,
+                :parser,
+                :input
 
   def self.run
     CLI.new.start
   end
 
   def initialize
+    @parser = Parser.new
     @command        = ""
     @queue_command  = ""
     @find_command   = ""
     @parameters     = ""
+    @input          = ""
     @queue ||= TheQueue.new
     @event_reporter ||= EventReporter.new(@queue)
   end
@@ -23,8 +27,10 @@ class CLI
     puts "Welcome to Entry Reporter"
     while command != 'q'
       puts "Enter your command:"
-      parts = process_input(gets.chomp)
-      assign_instructions(parts)
+      @input = gets.chomp
+      parts = @parser.process_input(input)
+      @parser.assign_instructions(parts)
+      assign_commands_and_params
       execute_instructions
     end
     puts "Quitting Event Reporter"
@@ -32,105 +38,11 @@ class CLI
 
   private
 
-  def process_input(input)
-    input.split
-  end
-
-  def process_find_input(input)
-    unless input.nil?
-      if @find_command == 'state'
-        input[0].upcase
-      else
-        input.map{|part| part.capitalize}.join
-      end
-    end
-  end
-
-  def assign_instructions(parts)
-    @command = parts[0]
-    if parts[0] == 'load'
-      if parts[1]
-        @parameters = Dir["./*/#{parts[1]}"].join('')
-      else
-        @parameters = './data/event_attendees.csv'
-      end
-    elsif parts[0] == 'find'
-      @find_command = parts[1]
-      @parameters = process_find_input(parts[2..-1])
-    elsif parts[0] == 'queue'
-      assign_queue_instructions(parts)
-    elsif parts[0] == 'help'
-      if parts[2]
-        assign_queue_help_instructions(parts)
-      elsif parts[1]
-        assign_help_instructions(parts)
-      else
-        @command = parts[0]
-      end
-    end
-  end
-
-  def assign_queue_instructions(parts)
-    case parts[1]
-    when 'count'
-      assign_queue_command(parts, 1)
-      assign_queue_parameters(parts, 1)
-    when 'clear'
-      assign_queue_command(parts, 1)
-      assign_queue_parameters(parts, 1)
-
-    when 'print'
-      assign_queue_command(parts, 1)
-      assign_queue_parameters(parts, 1)
-    end
-
-    case parts[1..2].join(" ")
-    when 'print by'
-      assign_queue_command(parts, 2)
-      assign_queue_parameters(parts, 2)
-
-    when 'save to'
-      assign_queue_command(parts, 2)
-      assign_queue_parameters(parts, 2)
-    end
-  end
-
-  def assign_queue_command(parts, n) # n is number of params to be added
-    @queue_command = parts[1] if n == 1
-    @queue_command = parts[1..2].join(" ") if n == 2
-  end
-
-  def assign_queue_parameters(parts, n)
-    @parameters = parts[2..-1].join(" ") if n == 1
-    @parameters = parts[3..-1].join(" ") if n == 2
-  end
-
-  def assign_help_instructions(parts)
-    case parts[1]
-    when 'find' then assign_help_parameter(parts, 1)
-    when 'load' then assign_help_parameter(parts, 1)
-    end
-  end
-
-  def assign_queue_help_instructions(parts)
-    case parts[1..2].join(" ")
-    when 'queue count' then assign_help_parameter(parts, 2)
-    when 'queue clear' then assign_help_parameter(parts, 2)
-    when 'queue print' then assign_help_parameter(parts, 2)
-    end
-
-    case parts[1..3].join(" ")
-    when 'queue print by' then assign_help_parameter(parts, 3)
-    when 'queue save to'  then assign_help_parameter(parts, 3)
-    end
-  end
-
-  def assign_help_parameter(parts, n)
-    case n
-    when 1 then @parameters = parts[1]
-    when 2 then @parameters = parts[1..2].join(" ")
-    when 3 then @parameters = parts[1..3].join(" ")
-    end
+  def assign_commands_and_params
+    @command        = @parser.command
+    @queue_command  = @parser.queue_command
+    @find_command   = @parser.find_command
+    @parameters     = @parser.parameters
   end
 
   def execute_instructions
--- lib/queue.rb
+++ lib/queue.rb
@@ -19,24 +19,26 @@ class TheQueue
   ["LAST NAME", "FIRST NAME", "EMAIL", "ZIPCODE", "CITY", "STATE", "ADDRESS", "PHONE"]
 
   def print_data_table(data=@current)
-    puts "LAST NAME".ljust(15) +
-    "FIRST NAME".ljust(15) +
-    "EMAIL".ljust(40) +
-    "ZIPCODE".ljust(15) +
-    "CITY".ljust(20) +
-    "STATE".ljust(15) +
-    "ADDRESS".ljust(25) +
-    "PHONE".ljust(15)
+    unless @current.empty?
+      puts "LAST NAME".ljust(15) +
+      "FIRST NAME".ljust(15) +
+      "EMAIL".ljust(40) +
+      "ZIPCODE".ljust(15) +
+      "CITY".ljust(20) +
+      "STATE".ljust(15) +
+      "ADDRESS".ljust(25) +
+      "PHONE".ljust(15)
 
-    data.each do |attendee|
-      puts attendee.last_name.ljust(15) +
-      attendee.first_name.ljust(15) +
-      attendee.email_address.ljust(40) +
-      attendee.zipcode.ljust(15) +
-      attendee.city.ljust(20) +
-      attendee.state.ljust(15) +
-      attendee.street.ljust(25) +
-      attendee.homephone.ljust(15)
+      data.each do |attendee|
+        puts attendee.last_name.ljust(15) +
+        attendee.first_name.ljust(15) +
+        attendee.email_address.ljust(40) +
+        attendee.zipcode.ljust(15) +
+        attendee.city.ljust(20) +
+        attendee.state.ljust(15) +
+        attendee.street.ljust(25) +
+        attendee.homephone.ljust(15)
+      end
     end
   end
 
@@ -52,9 +54,7 @@ class TheQueue
     CSV.open(file_name, 'wb') do |csv|
       csv << ["id", "RegDate", "last_name", "first_name", "email", "phone_number", "address", "city", "state", "zipode", ]
       if @current
-        @current.each do |attendee|
-          csv << attendee.data
-        end
+        @current.each {|attendee| csv << attendee.data}
       end
     end
     return "Your file has been saved to #{file_name}"
